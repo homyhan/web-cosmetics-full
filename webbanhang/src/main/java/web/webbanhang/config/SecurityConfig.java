605e9fd241cd22b03c4811696e2506eafc4d8d2c
homyhan <homyhan0616@gmail.com>
2024-05-13 22:05:21 +0700

    test security

---
+++ webbanhang/src/main/java/web/webbanhang/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+//package web.webbanhang.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//    @Bean
+//    public BCryptPasswordEncoder encodePwd(){
+//        return new BCryptPasswordEncoder();
+//    }
+//}
