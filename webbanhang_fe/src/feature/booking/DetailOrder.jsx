fe0c62daa552b6ac7fe9402cf7de0a4691edad71
nguyenvandan24 <20130219@st.hcmuaf.edu.vn>
2024-05-13 21:59:36 +0700

    Xem đơn hàng

--- webbanhang_fe/src/feature/booking/DetailOrder.jsx
+++ webbanhang_fe/src/feature/booking/DetailOrder.jsx
@@ -1,90 +1,53 @@
-
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
 import "../../components/style.css";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts, fetchCartById, fetchOrders, fetchDetailOrder } from "./thunk";
 import { useNavigate } from "react-router-dom";
-import { getProduct } from "./thunk";
 import { useParams } from 'react-router-dom';
-import { fetchProducts, addToCart, fetchCartById } from "./thunk";
 import Swal from "sweetalert2";
 
-const ProductDetail = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const params = useParams();
-  const productId = params?.id;
-  const selectedPro = useSelector((state) => state.booking.selectedPro);
-  const {user} = useSelector(state=>state.auth);
-  const isLoggedIn = localStorage.getItem("emailCosmetics") && localStorage.getItem("passcosmetics");
+const DetailOrder = () => {
+    const navigate = useNavigate();
+    const params = useParams();
+    const idOrder = params?.id;
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.booking.products);
+    const {user} = useSelector(state=>state.auth);
+    const orders = useSelector((state) => state.booking.orders);
+    const details = useSelector((state) => state.booking.details);
+    const [quantityProdCart, setQuantityProdCart] = useState(0);
 
-  useEffect(() => {
-    dispatch(getProduct(productId));
-  }, [dispatch, productId]);
+    const isLoggedIn = localStorage.getItem("emailCosmetics") && localStorage.getItem("passcosmetics");
+  
+    useEffect(() => {
+      dispatch(fetchProducts);
+      getQuantityProdInCart();
+      dispatch(fetchOrders(user.id)); 
+    }, [dispatch, user.id]);
+  
+    useEffect(() => {
+        dispatch(fetchDetailOrder(idOrder));
+      }, [dispatch, idOrder]);
 
-  const [quantity, setQuantity] = useState(1);
-  const increaseQuantity = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const decreaseQuantity = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
-
-  const [quantityProdCart, setQuantityProdCart] = useState(0);
-  useEffect(() => {
-    dispatch(fetchProducts);
-    getQuantityProdInCart();
-  }, [dispatch]);
-
-
-  const getQuantityProdInCart = async () => {
-    const idUser = user?.id;
-    if (idUser) {
-      const res = await dispatch(fetchCartById(idUser));   
-      setQuantityProdCart(res?.data?.length);
-    }
-  };
-
-  const handleAddToCart=async(id)=>{
-    const userId = user?.id;
-    if(userId){
-      const productId = id;
-    const res = await dispatch(addToCart({userId, productId, quantity}));
-    getQuantityProdInCart();
-    if(res.status=="200"){
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        title: res.data,
-        showConfirmButton: false,
-        timer: 1500
-    });
-    }else{
-      Swal.fire({
-        position: "center",
-        icon: "error",
-        title: res.data,
-        showConfirmButton: false,
-        timer: 1500
-    });
-    }
-    }else{
-      navigate("/login")
+    const getQuantityProdInCart = async () => {
+      const idUser = user?.id;
+      if (idUser) {
+        const res = await dispatch(fetchCartById(idUser));   
+        setQuantityProdCart(res?.data?.length);
+      }
+    };
+  
+    const handleToCartPage=async()=>{
+      navigate("/cart");
     }
     
-  }
+    const handleLogout = () => {
+        localStorage.removeItem("emailCosmetics");
+        localStorage.removeItem("passcosmetics");
+        navigate('/login');
+    };
 
-  const handleToCartPage=async()=>{
-    navigate("/cart");
-  }
-  const handleLogout = () => {
-    localStorage.removeItem("emailCosmetics");
-    localStorage.removeItem("passcosmetics");
-    navigate('/login');
-};
-
+    
   return (
     <div>
       <div className="py-1 bg-primary">
@@ -120,7 +83,11 @@ const ProductDetail = () => {
         id="ftco-navbar"
       >
         <div className="container">
-          <a className="navbar-brand">COSMETICS</a>
+        <a className="navbar-brand" style={{cursor:'pointer'}} onClick={() => {
+                    navigate("/");
+                  }}>
+              COSMETICS
+            </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -135,7 +102,7 @@ const ProductDetail = () => {
           <div className="collapse navbar-collapse" id="ftco-nav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <a className="nav-link">Home</a>
+                <a className="nav-link" style={{cursor:'pointer'}} onClick={()=>{navigate("/")}}>Home</a>
               </li>
               <li className="nav-item dropdown">
                 <a
@@ -165,7 +132,7 @@ const ProductDetail = () => {
                 <a className="nav-link">Contact</a>
               </li>
               <li className="nav-item cta cta-colored">
-              <a
+                <a
                   style={{cursor:'pointer'}}
                 onClick={() => {
                     handleToCartPage();
@@ -205,67 +172,43 @@ const ProductDetail = () => {
         </div>
       </nav>
 
-      <div className="product">
-      <div className="container mt-4">
-        <h2 className="title">Product Detail</h2>
-        <div className="row">
-          <div className="img col-md-6">
-            <img
-              src={selectedPro?.img}
-              alt="Product"
-              className="img-fluid"
-            />
-          </div>
-          <div className="col-md-6">
-            <h2 className="namePro">{selectedPro.nameProd}</h2>
-            <div className="rating mb-3">
-              <span className="star">&#9733;</span>
-              <span className="star">&#9733;</span>
-              <span className="star">&#9733;</span>
-              <span className="star">&#9733;</span>
-              <span className="star">&#9734;</span>
-            </div>
-            <p className="price mb-3">{selectedPro.price} VNĐ</p>
-            <p className="description mb-3">
-              {selectedPro.description}
-            </p>
-            <div className="input-group mb-3" style={{ maxWidth: "120px" }}>
-              <div className="input-group-prepend">
-                <button
-                  className="btn btn-outline-secondary"
-                  type="button"
-                  id="button-minus"
-                  onClick={decreaseQuantity}
-                >
-                  -
-                </button>
-              </div>
-              <input
-                type="text"
-                className="form-control text-center"
-                value={quantity}
-                placeholder=""
-                aria-label="Example text with button addon"
-                aria-describedby="button-addon1"
-              />
-              <div className="input-group-append">
-                <button
-                  className="btn btn-outline-secondary"
-                  type="button"
-                  id="button-plus"
-                  onClick={increaseQuantity}
-                >
-                  +
-                </button>
-              </div>
-            </div>
-            <button onClick={()=>{handleAddToCart(selectedPro.id)}} className="btn btn-primary btn-lg">Add to Cart</button>
-          </div>
-        </div>
+      {/* BANNER  */}     
+
+      {/* PRODUCT  */}
+      <div className="profile-container">
+      <div className="profile-main">
+        <h2>Chi tiết đơn hàng</h2>
+        <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">ID đơn hàng</th>
+                        <th scope="col">Tên sản phẩm</th>
+                        <th scope="col">Giá sản phẩm</th>
+                        <th scope="col">Số lượng</th>
+                        <th scope="col">Tổng tiền</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {details.map((detail, index) => (
+            <tr key={index}>
+              <th>{index + 1}</th>
+              <td>{detail.orders?.id}</td>
+              <td>{detail.product.nameProd}</td>
+              <td>{detail.price}</td>
+              <td>{detail.quantity}</td>
+              <td>{detail.orders.totalPrice}</td>
+            </tr>
+          ))}
+            
+                </tbody>
+            </table>
+            <button className="btn btn-primary" onClick={()=>{navigate("/user/profile")}}>Quay lại</button>
       </div>
-      </div>
+    </div>
+
     </div>
   );
 };
 
-export default ProductDetail;
+export default DetailOrder;
