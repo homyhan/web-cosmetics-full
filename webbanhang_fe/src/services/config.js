dbbe47b6d0db3ec7988812426bdc33d31fdd58e4
homyhan <98325510+homyhan@users.noreply.github.com>
2024-06-23 19:30:21 +0700

    change api

--- webbanhang_fe/src/services/config.js
+++ webbanhang_fe/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL ="http://localhost:8080";
-// const BASE_URL ="https://web-cosmetics-full-production.up.railway.app";
+// const BASE_URL ="http://localhost:8080";
+const BASE_URL ="https://web-cosmetics-full-production-c585.up.railway.app";
 export const http = axios.create({
     baseURL: BASE_URL
 })
